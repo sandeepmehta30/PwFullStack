0647ff3b1f9b1fabaff3b96394e4c96740712b50
sandeepmehta30 <112331347+sandeepmehta30@users.noreply.github.com>
2024-02-04 12:34:20 +0530

    6TH COMMIT

--- Auth With Express Assignment - Copy/Server/middleware/login.validator.js
+++
@@ -1,8 +0,0 @@
-exports.loginValidator = (req,res,next)=>{
-    const {username,password} = req.body;
-    if(req.body&&username&&password){
-        next()
-    }else{
-        res.status(404).send({msg:"All input fields are required"})
-    }
-}
--- Auth With Express Assignment - Copy/Server/middleware/signup.validator.js
+++
@@ -1,9 +0,0 @@
-exports.signupValidator = (req,res,next) =>{
-    const {name,email,password,bio,username} = req.body;
-
-    if(req.body && name && email && password && bio && username){
-        next()
-    }else{
-        res.status(404).send({msg:"all Input Fields are required"})
-    }
-}
