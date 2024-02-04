c1d27d34f50f39c343ec3b0d9dd0436a00f13818
sandeepmehta30 <112331347+sandeepmehta30@users.noreply.github.com>
2024-02-04 11:53:38 +0530

    second commit

---
+++ JsFundamental/array1.js
@@ -0,0 +1,22 @@
+let a = [1,2,3,4, "Suman", false];  
+
+console.log(a);
+
+let b= new Array("abc", "def", "ghi");
+console.log(b);
+
+///array is collection of data
+
+/**
+ * 
+ * given array -> [11,21,31,41,51]
+ * 
+ * index/pos-> [0,1,2,3,4] //last element index = arr.lenth -1
+ * 
+ */
+
+
+console.log(a[4]);
+
+a[4]=5;
+console.log(a);
